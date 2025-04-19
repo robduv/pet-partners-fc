bd6192c0ca956b17a391f265be1a1f37b08091e5
Rob Duval <robduv@gmail.com>
2025-04-19 17:40:01 -0400

    Correct positioning of header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -42,8 +42,8 @@ const events: CalendarEventProps[] = [
     <div
       class="flex flex-col gap-4 rounded-xl border border-surface-200 bg-surface-0 p-6 text-base text-surface-600 dark:border-surface-700 dark:bg-surface-900 dark:text-surface-400"
     >
+      <span class="text-lg font-bold">Upcoming Events</span>
       <div class="flex items-center justify-between">
-        <span class="text-lg font-bold">Upcoming Events</span>
         <div class="flex flex-col gap-3">
           <CalendarEvent
             v-for="(event, index) in events"
