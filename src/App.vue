a4a2fdae0e55e4ac5e170cfd2694dae96fd2e335
Rob Duval <robduv@gmail.com>
2025-04-20 00:18:37 -0400

    Simplify styling

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,19 @@ import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col gap-6 bg-surface-50 p-6 dark:bg-surface-950">
+  <div
+    class="mx-auto flex min-h-screen w-full max-w-7xl flex-col gap-6 bg-surface-50 p-6 dark:bg-surface-950"
+  >
     <div
-      class="mx-auto w-full max-w-7xl rounded-xl border border-surface-200 bg-surface-0 p-6 text-surface-600 dark:border-surface-700 dark:bg-surface-900 dark:text-surface-400"
+      class="rounded-xl border border-surface-200 bg-surface-0 p-6 text-surface-600 dark:border-surface-700 dark:bg-surface-900 dark:text-surface-400"
     >
       <AppHeader />
     </div>
-    <div class="mx-auto flex w-full max-w-7xl flex-1 flex-col gap-6">
+    <div class="flex flex-1 flex-col gap-6">
       <RouterView />
     </div>
     <div
-      class="mx-auto w-full max-w-7xl rounded-xl border border-surface-200 bg-surface-0 p-6 dark:border-surface-700 dark:bg-surface-900"
+      class="rounded-xl border border-surface-200 bg-surface-0 p-6 text-surface-600 dark:border-surface-700 dark:bg-surface-900 dark:text-surface-400"
     >
       <AppFooter />
     </div>
