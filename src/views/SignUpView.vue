f211d81aaed55746e22d0154189fa88d65c89e73
Rob Duval <robduv@gmail.com>
2025-04-20 18:41:24 -0400

    Refine styling on contact and signup views

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col gap-4 rounded-xl border border-surface-200 bg-surface-0 p-6 text-base text-surface-600 dark:border-surface-700 dark:bg-surface-900 dark:text-surface-400"
+    class="flex flex-col gap-4 rounded-xl border border-slate-300 bg-white p-4 text-base text-slate-700"
   >
     <iframe
       src="https://docs.google.com/forms/d/e/1FAIpQLSeFwBGJt19H6AXSgwm6iGPcTYzgQv5N7sT9sGZbUwgT11kedQ/viewform?embedded=true"
