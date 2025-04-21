e6d011054fc5816a96b06a38010b2750f52b4f52
Rob Duval <robduv@gmail.com>
2025-04-20 21:41:47 -0400

    Make picture overflow visible

--- src/components/TeamMember.vue
+++ src/components/TeamMember.vue
@@ -11,10 +11,14 @@ const teamMember = defineProps<TeamMemberProps>()
 
 <template>
   <div
-    class="relative flex min-h-24 items-center gap-2 overflow-hidden rounded-lg border border-slate-300 bg-slate-100 text-slate-700"
+    class="relative m-2 ml-6 flex min-h-18 items-center gap-2 overflow-visible rounded-lg border border-slate-300 bg-slate-100 pl-4 text-slate-700"
   >
-    <img :src="teamMember.imageUrl" alt="Team Member" class="absolute h-32 w-32" />
-    <div class="flex flex-col pr-2 pl-34">
+    <img
+      :src="teamMember.imageUrl"
+      alt="Team Member"
+      class="absolute -left-6 h-24 w-24 rounded-full"
+    />
+    <div class="flex flex-col pr-2 pl-16">
       <div class="font-bold">{{ teamMember.name }}</div>
       <div class="text-sm italic">{{ teamMember.role }}</div>
       <p class="text-sm">{{ teamMember.bio }}</p>
